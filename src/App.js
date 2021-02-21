b52f211587a1ba6469e6f64a850361b7df985772
Nabeel <nabeelfarid@gmail.com>
2021-02-21 06:21:36 +0500

    Added Charts

--- src/App.js
+++ src/App.js
@@ -5,37 +5,14 @@ import CountUp from 'react-countup'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import SettingsIcon from '@material-ui/icons/Settings';
 import styles from './App.module.css'
+import { Bar, Doughnut, Line } from 'react-chartjs-2';
 
-const CovidSummaryStats = ({ country }) => {
-  const [stats, setStats] = useState({
-    infected: 0,
-    recovered: 0,
-    deaths: 0,
-    lastUpdate: new Date()
-  })
+const CovidSummaryStats = ({ stats }) => {
 
-  useEffect(() => {
-    // https://covid19.mathdro.id/api/countries
-    console.log('summary country:', country)
-    axios.get(
-      country ?
-        `https://covid19.mathdro.id/api/countries/${country}` :
-        'https://covid19.mathdro.id/api/'
-    )
-      .then((res) => {
-        // console.log(res)
-        setStats({
-          infected: Number(res.data.confirmed.value),
-          recovered: Number(res.data.recovered.value),
-          deaths: Number(res.data.deaths.value),
-          lastUpdate: new Date(res.data.lastUpdate)
-        })
-      })
-  }, [country])
 
   return (
 
-    <Grid container spacing={2} justify="center">
+    <Grid container spacing={2} alignItems="center" justify="center">
       <Grid item xs={12} sm={4} md={3}>
         <Card className={styles.infected}>
           <CardContent>
@@ -88,9 +65,9 @@ const CovidSummaryStats = ({ country }) => {
 const CountryPicker = ({ country, selectedCountryHandler }) => {
   const [countries, setCountries] = useState([])
   useEffect(() => {
-    axios.get('https://covid19.mathdro.id/api/countries')
+    axios.get('https://disease.sh/v3/covid-19/countries')
       .then((res) => {
-        setCountries(res.data.countries.map(country => country.name))
+        setCountries(res.data.map(obj => obj.country))
       })
   }, [])
 
@@ -116,9 +93,97 @@ const CountryPicker = ({ country, selectedCountryHandler }) => {
   )
 }
 
-const Charts = ({ country }) => {
+const Charts = ({ stats }) => {
+
+  const data = {
+    datasets: [{
+      data: [stats.infected, stats.recovered, stats.deaths],
+      backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+      borderColor: ['rgba(0, 0, 255, 0.6)', 'rgba(0, 255, 0, 0.6)', 'rgba(255, 0, 0, 0.6)'],
+      hoverBackgroundColor: ['rgba(0, 0, 255, 0.7)', 'rgba(0, 255, 0, 0.7)', 'rgba(255, 0, 0, 0.7)'],
+      hoverBorderColor: ['rgba(0, 0, 255, 0.8)', 'rgba(0, 255, 0, 0.8)', 'rgba(255, 0, 0, 0.8)'],
+    }],
+    labels: ['Infected', 'Recovered', 'Deaths']
+  }
+
+  const options = {
+    legend: {
+      display: false
+    }
+
+  }
+
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+
+    let url = `https://disease.sh/v3/covid-19/historical/${stats.country ? stats.country : 'all'}?lastdays=365`
+    console.log('url', url)
+    axios.get(url)
+      .then((res) => {
+        const resData = stats.country ? res.data.timeline : res.data;
+        console.log('resData', resData)
+        let data = Object.entries(resData.cases).map(
+          ([key, value]) => {
+            return {
+              date: new Date(key),
+              infected: Number(value),
+              recovered: Number(resData.recovered[key]),
+              deaths: Number(resData.deaths[key])
+            }
+          })
+        console.log('daily data', data)
+
+        setDailyData(data)
+      })
+  }, [stats.country])
+
+
   return (
-    <></>
+    <Box m={2}>
+      <Grid container spacing={4} alignItems="center" justify="center">
+        <Grid item md={1} ></Grid>
+        <Grid item md={5}>
+          <Bar data={data} options={options} />
+        </Grid>
+        <Grid item md={5}>
+          <Doughnut data={data} options={options} />
+        </Grid>
+        <Grid item md={1}></Grid>
+        <Grid item xs={12} md={10}>
+          <Line
+            data={{
+              labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+              datasets: [{
+                data: dailyData.map((data) => data.infected),
+                label: 'Infected',
+                borderColor: 'rgba(0, 0, 255, 0.5)',
+                fill: false,
+              }, {
+                data: dailyData.map((data) => data.deaths),
+                label: 'Deaths',
+                borderColor: 'rgba(255, 0, 0, 0.5)',
+                fill: false,
+              }, {
+                data: dailyData.map((data) => data.recovered),
+                label: 'Recovered',
+                borderColor: 'rgba(0, 255, 0, 0.5)',
+                fill: false,
+              },
+              ],
+            }}
+            options={{
+              title: {
+                display: true,
+                text: `Historical data over last year: ${stats.country ? stats.country : 'Worldwide'}`
+              },
+              legend: {
+                display: false
+              }
+            }}></Line>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
@@ -126,6 +191,33 @@ const Charts = ({ country }) => {
 const App = () => {
 
   const [country, setCountry] = useState('');
+
+  const [stats, setStats] = useState({
+    country: '',
+    infected: 0,
+    recovered: 0,
+    deaths: 0,
+    lastUpdate: new Date()
+  })
+
+  useEffect(() => {
+    console.log('summary country:', country)
+    axios.get(
+      country ?
+        `https://disease.sh/v3/covid-19/countries/${country}` :
+        'https://disease.sh/v3/covid-19/all'
+    )
+      .then((res) => {
+        console.log(res)
+        setStats({
+          country: res.data.country,
+          infected: Number(res.data.cases),
+          recovered: Number(res.data.recovered),
+          deaths: Number(res.data.deaths),
+          lastUpdate: new Date(res.data.updated)
+        })
+      })
+  }, [country])
 
   const selectedCountryHandler = (selectedCountry) => {
     setCountry(selectedCountry)
@@ -142,7 +234,7 @@ const App = () => {
               </IconButton>
             </Tooltip>
             <Tooltip title="Covid-19 API Source">
-              <IconButton color="inherit" href="https://covid19.mathdro.id/api/" target="_blank" rel="noopener noreferrer">
+              <IconButton color="inherit" href="https://corona.lmao.ninja/" target="_blank" rel="noopener noreferrer">
                 <SettingsIcon></SettingsIcon>
               </IconButton>
             </Tooltip>
@@ -151,17 +243,16 @@ const App = () => {
 
       </AppBar>
       <Box m={2}>
-        <Grid container spacing={2} alignItems="center" justify="center">
-          <Grid item xs={12}></Grid>
-
+        <Grid container spacing={4} alignItems="center" justify="center">
           <Grid item xs={12}>
-            <CovidSummaryStats country={country} />
-          </Grid>
-          <Grid item>
             <CountryPicker country={country} selectedCountryHandler={selectedCountryHandler} />
           </Grid>
-          <Grid item>
-            <Charts country={country} />
+          <Grid item xs={12}>
+            <CovidSummaryStats stats={stats} />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Charts stats={stats} />
           </Grid>
         </Grid>
       </Box>
